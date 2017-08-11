798b50c2a218b2eedf32ec0c7f228fe82c2b1598
David Wilson <davidtheadmiral@gmail.com>
2017-08-11 11:01:33 -0700

    first model test passing, data type verification complete for Inventory model.

---
+++ SonOfCodTests/ModelTests/InventoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests
+{
+    public class PlayerTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var inventoryItem = new Inventory();
+            inventoryItem.Description = "Dude this thing can break your finger. Watch out.";
+            inventoryItem.Name = "Mantis Shrimp";
+            inventoryItem.Cost = 12.99M;
+
+            //Act
+            var result = inventoryItem.Description;
+            var namez = inventoryItem.Name;
+            var cawst = inventoryItem.Cost;
+
+            //Assert
+            Assert.Equal("Dude this thing can break your finger. Watch out.", result);
+            Assert.Equal("Mantis Shrimp", namez);
+            Assert.Equal(12.99M, cawst);
+        }
+    }
+}
